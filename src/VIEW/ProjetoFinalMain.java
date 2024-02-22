30f7d798e3453488f9cae56669de4f735d336552
PedroRezende14 <pedro.rezende14@outlook.com>
2024-02-22 10:56:52 -0300

    Primeiro comit do projeto.

---
+++ src/VIEW/ProjetoFinalMain.java
@@ -0,0 +1,16 @@
+package VIEW;
+import java.sql.SQLException;
+
+import BO.*;
+import DTO.*;
+import MYSQL.*;
+import Conexao.Conexao;
+public class ProjetoFinalMain {
+	
+
+
+public static void main(String[] args) {
+		LoginMain lm = new LoginMain();
+		lm.inicio();
+	}
+}
